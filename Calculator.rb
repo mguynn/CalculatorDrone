adafa7f9f313bbcfda0ab28dda09470dd6755dc1
bourjasc <bourjasc@gmail.com>
2015-06-25 19:36:44 +0000

    Initial Calculator

---
+++ Calculator.rb
@@ -0,0 +1,88 @@
+def multiplication
+  puts "Pick your first number."
+  a = gets.chomp 
+  puts "Pick your second number."
+  b = gets.chomp
+  a.to_i * b.to_i
+end
+
+puts "Hello, welcome to the DRONE Calculator."
+puts "Choose a function listed and type it exactly as shown: Addition, Subtraction, Multiplication, Division"
+# while true 
+#   input = gets.chomp
+#   if input == 'Addition'
+#     break
+#   end
+# end
+# puts "What is the first number?" 
+# x = gets.chomp.to_i 
+# puts "What is the second number?"
+# y = gets.chomp.to_i
+# puts x + y
+
+# while true 
+#   input = gets.chomp
+#   if input == 'Subtraction'
+#   end
+# end
+# puts "What is the first number?" 
+# z = gets.chomp.to_i 
+# puts "What is the second number?"
+# xy = gets.chomp.to_i
+# puts z - xy
+
+# while true 
+#   input = gets.chomp
+#   puts input
+#   if input == 'Subtraction'
+#     break
+#   end
+# end
+# puts gets.chomp.to_i - gets.chomp.to_i
+
+
+# while true 
+#   input = gets.chomp
+#   if input == 'Multiplication'
+#     break
+#   end
+# end
+# puts "What is the first number?" 
+# x = gets.chomp.to_i 
+# puts "What is the second number?"
+# y = gets.chomp.to_i
+# puts x * y
+ 
+
+#puts "What is the first number?" 
+# a = gets.chomp.to_i 
+# puts "What is the second number?"
+# b = gets.chomp.to_i
+# puts a * b
+#     break 
+#   end
+# end
+
+def multiplication
+  puts "Pick your first number."
+  a = gets.chomp 
+  puts "Pick your second number."
+  b = gets.chomp
+  a.to_i * b.to_i
+end
+
+def addition
+  puts "Pick you first number."
+  c = gets.chomp
+  puts "Pick your second number."
+  d = gets.chomp
+  c.to_i + d.to_i
+end
+
+def subtraction
+  puts "Pick you first number."
+  x = gets.chomp
+  puts "Pick your second number."
+  y = gets.chomp
+  x.to_i + y.to_i
+  
